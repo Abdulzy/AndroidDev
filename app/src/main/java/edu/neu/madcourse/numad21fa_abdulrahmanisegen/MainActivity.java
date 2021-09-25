2ec9e8ab2e28e21b8678b67554f246d511fbe8a5
abdul <abdul.isegen@gmail.com>
2021-09-25 15:58:19 -0400

    changed functionality of button

--- app/src/main/java/edu/neu/madcourse/numad21fa_abdulrahmanisegen/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad21fa_abdulrahmanisegen/MainActivity.java
@@ -7,23 +7,27 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements View.OnClickListener{
 
     protected Button toastButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toastButton = findViewById(R.id.about);
-        toastButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v)
-            {
+    }
+
+    public void onClick(View view)
+    {
+        switch (view.getId()) {
+            case R.id.about:
                 Toast.makeText(getApplicationContext(),
                         "Abdulrahman Isegen \n isegen.a@northeasten.edu",
                         Toast.LENGTH_LONG)
                         .show();
-            }
-        });
+                break;
+        }
+
     }
+
 }
