fd0536c545a9b8daab18db3ddc3d29e3b296d7b4
abdul <abdul.isegen@gmail.com>
2021-09-26 11:13:27 -0400

    made on touch listener

--- app/src/main/java/edu/neu/madcourse/numad21fa_abdulrahmanisegen/ClickyActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad21fa_abdulrahmanisegen/ClickyActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ClickyActivity extends AppCompatActivity
@@ -13,6 +14,74 @@ public class ClickyActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clicky);
+        Button button_a = findViewById(R.id.Button_A);
+        Button button_b = findViewById(R.id.Button_B);
+        Button button_c = findViewById(R.id.Button_C);
+        Button button_d = findViewById(R.id.Button_D);
+        Button button_e = findViewById(R.id.Button_E);
+        Button button_f = findViewById(R.id.Button_F);
+
+        button_a.setOnLongClickListener(
+                new Button.OnLongClickListener(){
+                    public boolean onLongClick(View view){
+                        TextView display = findViewById(R.id.clickyDisplay);
+                        display.setText("Pressed: A");
+                        return false;
+                    }
+                }
+        );
+
+        button_b.setOnLongClickListener(
+                new Button.OnLongClickListener(){
+                    public boolean onLongClick(View view){
+                        TextView display = findViewById(R.id.clickyDisplay);
+                        display.setText("Pressed: B");
+                        return false;
+                    }
+                }
+        );
+
+        button_c.setOnLongClickListener(
+                new Button.OnLongClickListener(){
+                    public boolean onLongClick(View view){
+                        TextView display = findViewById(R.id.clickyDisplay);
+                        display.setText("Pressed: C");
+                        return false;
+                    }
+                }
+        );
+
+        button_d.setOnLongClickListener(
+                new Button.OnLongClickListener(){
+                    public boolean onLongClick(View view){
+                        TextView display = findViewById(R.id.clickyDisplay);
+                        display.setText("Pressed: D");
+                        return false;
+                    }
+                }
+        );
+
+        button_e.setOnLongClickListener(
+                new Button.OnLongClickListener(){
+                    public boolean onLongClick(View view){
+                        TextView display = findViewById(R.id.clickyDisplay);
+                        display.setText("Pressed: E");
+                        return false;
+                    }
+                }
+        );
+
+        button_f.setOnLongClickListener(
+                new Button.OnLongClickListener(){
+                    public boolean onLongClick(View view){
+                        TextView display = findViewById(R.id.clickyDisplay);
+                        display.setText("Pressed: F");
+                        return false;
+                    }
+                }
+        );
+
+
     }
 
     public void onClick(View view)
@@ -20,24 +89,11 @@ public class ClickyActivity extends AppCompatActivity
         TextView display = findViewById(R.id.clickyDisplay);
         switch (view.getId()) {
             case R.id.Button_A:
-                display.setText("Pressed:A");
-                break;
             case R.id.Button_B:
-                display.setText("Pressed:B");
-                break;
             case R.id.Button_C:
-                display.setText("Pressed:C");
-                break;
             case R.id.Button_D:
-                display.setText("Pressed:D");
-                break;
             case R.id.Button_E:
-                display.setText("Pressed:E");
-                break;
             case R.id.Button_F:
-                display.setText("Pressed:F");
-                break;
-            default:
                 display.setText("Pressed:-");
                 break;
         }
